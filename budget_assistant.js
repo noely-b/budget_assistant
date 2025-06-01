6a947a469715282e3a30e7f486344b1159df0985
Noely <noely.bosque@gmail.com>
2025-05-31 23:59:14 -0400

    Basic Biz Budget Input

--- budget_assistant.js
+++ budget_assistant.js
@@ -4,5 +4,5 @@ let monthlyRevenue = 7000;
 let monthlyExpenses = 5000;
 let netMonthlyCashFlow = monthlyRevenue - monthlyExpenses;
 let currentBalance = initialCapital + netMonthlyCashFlow;
-let profitability = true if cashFlow > 0 else false;
+let profitability = true if netMonthlyCashFlow > 0 else false;
 console.log(startup, netMonthlyCashFlow, currentBalance, profitability);
